6fb9261e47dc1ad26376fe29e3654d1cf1360c54
Thunderstrom11 <dmchamendy@umav.edu.ni>
2025-08-29 08:44:57 -0600

    Sesion#3_v3

---
+++ Dowhile/Program.cs
@@ -0,0 +1,13 @@
+﻿// Muestra un menu que diga 1. Bienvenido, 2. Saludo 3. Salir
+
+string opcion;
+
+do
+{
+    Console.WriteLine("1. Bienvenido.");
+    Console.WriteLine("2. Saluda.");
+    Console.WriteLine("3. Salir.");
+    Console.WriteLine("Digite su opcion: ");
+    opcion = Console.ReadLine();
+    
+} while (opcion != "3");

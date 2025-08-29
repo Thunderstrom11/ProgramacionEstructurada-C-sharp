0d00ad8053f7f84d4e1dfb48e0856c448d564e6d
Thunderstrom11 <dmchamendy@umav.edu.ni>
2025-08-29 08:15:47 -0600

    Sesion#3_v2

---
+++ While/Program.cs
@@ -0,0 +1,20 @@
+﻿/* lectura de numero hasta que sea negativo , mostrar la suma de todos hasta que sea
+negativo*/
+
+int num,suma = 0;
+string entrada;
+Console.Write("Dime un #: ");
+entrada = Console.ReadLine();
+num = Convert.ToInt32(entrada);
+
+while (num >= 0 && suma <= 100)
+{
+    suma += num;
+    if (suma >= 100)
+        break;
+    Console.Write("Dime un #: ");
+    entrada = Console.ReadLine();
+    num = Convert.ToInt32(entrada);
+}
+
+Console.WriteLine($"La suma total es {suma}");

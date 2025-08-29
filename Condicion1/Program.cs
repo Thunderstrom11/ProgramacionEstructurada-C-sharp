bfbb15f41b15955fee489da77b1ba7274fb12c49
Thunderstrom11 <dmchamendy@umav.edu.ni>
2025-08-28 18:49:06 -0600

    Prueba3

--- Condicion1/Program.cs
+++ Condicion1/Program.cs
@@ -1,11 +1,11 @@
-﻿//Verificar si es mayor de esdad
+﻿//Verificar si es mayor de edad
 
 int edad;
 
 Console.Write("Dime tu edad: ");
 edad = int.Parse(Console.ReadLine());
 
-if (edad >= 18)
+if (edad >= 18) 
 {
     Console.WriteLine("Eres mayor de edad.");
 }

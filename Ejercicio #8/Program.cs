10137d02b091d6ce3100c5fdf0e9b2c07e8c3406
Diego Chamendy <dmchamendy@uamv.edu.ni>
2025-09-10 00:11:03 -0600

    Add files via upload

---
+++ Ejercicio #8/Program.cs
@@ -0,0 +1,31 @@
+﻿//Validación de contraseña (do-while)
+//Crea un programa que pida al usuario una contraseña y repita la solicitud hasta que ingrese la correcta. Usa un bucle do while
+
+namespace Ejercicio__8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string contrasena_correcta = "C#1201", contrasena_ingresada;
+
+            Console.WriteLine("Validador de contraseña\n");
+
+            do
+            {
+                Console.Write("Ingrese la contraseña :");
+                contrasena_ingresada = Console.ReadLine();
+
+                if (contrasena_ingresada != contrasena_correcta)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Contraseña incorrecta. Intente de nuevo.\n");
+                }
+
+            }
+            while (contrasena_ingresada != contrasena_correcta);
+
+            Console.WriteLine("Contraseña correcta. Acceso concedido.\n");
+        }
+    }
+}
---
+++ Ejercicio #9/Program.cs
@@ -0,0 +1,61 @@
+﻿//Número mayor entre tres valores
+//Solicita tres números diferentes al usuario y usa estructuras if para determinar cuál es el mayor.
+
+namespace Ejercicio__9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1, numero2, numero3;
+
+            Console.WriteLine("Determinador de número mayor entre tres valores\n");
+
+            try
+                {
+                Console.Write("Ingrese el primer número: ");
+                numero1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Ingrese el segundo número: ");
+                numero2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Ingrese el tercer número: ");
+                numero3 = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.Clear();
+                Console.WriteLine("Error:Entrada no válida. Por favor, ingrese un número válido.");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine($"Error:{ex.Message}");
+                return;
+            }
+            if (numero1 == numero2 || numero1 == numero3 || numero2 == numero3)
+            {
+                Console.Clear();
+                Console.WriteLine("Error:Los números deben ser diferentes entre sí.");
+                return;
+            }
+
+            if (numero1 > numero2 && numero1 > numero3)
+            {
+                Console.Clear();
+                Console.WriteLine($"El número mayor es el primer numero: {numero1}\n");
+            }
+            else if (numero2 > numero1 && numero2 > numero3)
+            {
+                Console.Clear();
+                Console.WriteLine($"El número mayor es el segundo número: {numero2}\n");
+            }
+            else // (numero3 > numero1 && numero3 > numero2)
+            {
+                Console.Clear();
+                Console.WriteLine($"El número mayor es el tercer número: {numero3}\n");
+            }
+        }
+    }
+}

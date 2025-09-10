5661f0f7badb006cdc49d3d51d33046421b5f957
Diego Chamendy <dmchamendy@uamv.edu.ni>
2025-09-10 00:09:09 -0600

    Add files via upload

---
+++ Ejercicio #4/Program.cs
@@ -0,0 +1,44 @@
+﻿//4)Tabla de multiplicar con do-while
+//Solicita un número al usuario y usa un bucle do while para mostrar su tabla de multiplicar del 1 al 10
+
+namespace Ejercicio__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+            int multiplicacion = 1;
+
+            Console.Write("Ingrese un número para ver su tabla de multiplicar: ");
+
+            try
+            {
+                numero = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.Clear();
+                Console.WriteLine("Error:Entrada no válida. Por favor, ingrese un número válido.");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine($"Error:{ex.Message}");
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"Tabla de multiplicar del {numero}:");
+            Console.ResetColor();
+
+            do
+            {
+                Console.WriteLine($"{Math.Round(numero,2)} x {multiplicacion} = {Math.Round(numero * multiplicacion,2)}");
+                multiplicacion++;
+            }
+            while (multiplicacion <= 10);
+        }
+    }
+}
---
+++ Ejercicio #5/Program.cs
@@ -0,0 +1,50 @@
+﻿//Contar números pares e impares
+//Pide al usuario que ingrese 10 números. Usa un bucle for y estructuras if para contar cuántos son pares y cuántos impares.
+
+namespace Ejercicio__5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int control, num_pares = 0, num_impares = 0;
+            double numero;
+
+            Console.WriteLine("Contador de números pares e impares\n");
+
+            for (control = 1; control <= 10; control++) 
+            {
+                Console.Write($"Ingrese el {control} número : ");
+                try
+                {
+                    numero = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Error:Entrada no válida. Por favor, ingrese un número entero.");
+                    control--; // resta 1 al control para no avanzar en el rango del bucle
+                    continue; 
+                }
+                catch (Exception ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Error:{ex.Message}");
+                    control--; 
+                    continue; 
+                }
+                if (numero % 2 == 0)
+                {
+                    num_pares++;
+                }
+                else
+                {
+                    num_impares++;
+                }
+
+            }
+            Console.Clear();
+            Console.WriteLine($"Se han ingresado {num_pares} números pares y {num_impares} números impares\n");
+        }
+    }
+}

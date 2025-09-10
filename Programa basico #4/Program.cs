6fe392efdeee45d452f15ff5495c0f3b338da219
Diego Chamendy <dmchamendy@uamv.edu.ni>
2025-09-10 00:13:53 -0600

    Add files via upload

---
+++ Programa basico #4/Program.cs
@@ -0,0 +1,31 @@
+﻿//Desarrolle un programa que calcule el volumen total en metros cúbicos de agua recolectada en un tanque en una finca de Masaya,
+//utilizando como entradas la longitud, el ancho y la altura del tanque, todos proporcionados por el usuario.
+
+namespace Programa_basico__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double longitud, ancho, altura, volumen_total;
+
+            Console.WriteLine ("Programa de calculo de volumen total en metros cúbicos de agua recolectada en un tanque en una finca de Masaya\n");
+
+            Console.Write("Ingrese la longitud del tanque en metros:");
+            longitud = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.Write("Ingrese el ancho del tanque en metros:");
+            ancho = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.Write("Ingrese la altura del tanque en metros:");
+            altura = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            volumen_total = longitud * ancho * altura;
+
+            Console.WriteLine($"El volumen total de agua recolectada en el tanque con una longitud de {longitud} metros, un ancho de {ancho} metros y una altura de {altura} metros es de {Math.Round(volumen_total, 2)} metros cúbicos.");
+        }
+    }
+}
---
+++ Programa basico #5/Program.cs
@@ -0,0 +1,29 @@
+﻿//Desarrolle un programa que calcule la cantidad total de baldes de leche producida en un día en una lechería de Estelí,
+//utilizando como entradas el volumen promedio por vaca en litros y el número total de vacas ordeñadas,
+//ambos proporcionados por el usuario (1 balde = 20 litros).
+
+namespace Programa_basico__5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double volumen_promedio_por_vaca, total_baldes ;
+            int numero_total_de_vacas;
+
+            Console.WriteLine("Programa de calculo de cantidad total de baldes de leche producida en un día en una lechería de Estelí\n");
+
+            Console.Write("Ingrese el volumen promedio por vaca en litros:");
+            volumen_promedio_por_vaca = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.Write("Ingrese el número total de vacas ordeñadas:");
+            numero_total_de_vacas = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            total_baldes = (volumen_promedio_por_vaca * numero_total_de_vacas) / 20; // 1 balde = 20 litros
+
+            Console.WriteLine($"La cantidad total de baldes de leche producida en un día en la lechería con un volumen promedio por vaca de {volumen_promedio_por_vaca} litros y un número total de {numero_total_de_vacas} vacas ordeñadas es de {Math.Round(total_baldes, 2)} baldes.");
+        }
+    }
+}
